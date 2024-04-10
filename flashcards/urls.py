8001ad323efe1787f1cf83555cd93e99b54e6a99
sebmurphy <34035696+sebmurphy@users.noreply.github.com>
2024-04-10 15:42:58 +0200

    Update urls.py

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,10 +1,18 @@
-# flashcards/urls.py
+# cards/urls.py
+from typing import List, Any
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+# Removed: from django.views.generic import TemplateView
+
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.core.wsgi import get_wsgi_application
+from django.conf.urls.static import static
+
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("cards.urls")),
-]
-
+    path("", views.welcome_page_view, name="welcome'"),
+    path('existing-cards/', views.existing_cards_view, name='existing_cards'),
+    path('create-cards/', views.create_cards_view, name='create_cards'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
